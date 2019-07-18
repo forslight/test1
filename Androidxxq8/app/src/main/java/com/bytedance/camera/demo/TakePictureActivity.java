44dfd167a31e0c45338964922c2e7dee8d2dc1b6
forslight <3220978310@qq.com>
2019-07-18 12:28:58 +0800

    hw8

---
+++ Androidxxq8/app/src/main/java/com/bytedance/camera/demo/TakePictureActivity.java
@@ -0,0 +1,150 @@
+package com.bytedance.camera.demo;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bytedance.camera.demo.utils.Utils;
+
+import java.io.File;
+
+public class TakePictureActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_EXTERNAL_STORAGE = 101;
+    private File imageFile;
+    String[] permissions = new String[] {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_take_picture);
+        imageView = findViewById(R.id.img);
+        findViewById(R.id.btn_picture).setOnClickListener(v -> {
+            if (Utils.isPermissionsReady(TakePictureActivity.this,permissions)) {
+                takePicture();
+            } else {
+                //todo 在这里申请相机、存储的权限
+                Utils.reuqestPermissions(TakePictureActivity.this,permissions,REQUEST_EXTERNAL_STORAGE);
+            }
+        });
+
+    }
+
+    private void takePicture() {
+        //todo 打开相机
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        imageFile = Utils.getOutputMediaFile(Utils.MEDIA_TYPE_IMAGE);
+        if(imageFile != null) {
+            Uri fileUri =
+                    FileProvider.getUriForFile(this,"com.bytedance.camera.demo", imageFile);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+        //todo 处理返回数据
+            setPic();
+        }
+    }
+
+    private void setPic() {
+        //todo 根据imageView裁剪
+        //todo 根据缩放比例读取文件，生成Bitmap
+
+        //todo 如果存在预览方向改变，进行图片旋转
+
+        //todo 如果存在预览方向改变，进行图片旋转
+        //todo 显示图片
+
+        int targetW = imageView.getWidth();
+        int targetH = imageView.getHeight();
+        //to.do 根据缩放比例读取文件，生成Bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(imageFile.getAbsolutePath(), bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+        int scaleFactor = Math.min(photoH / targetH, photoW / targetW);
+
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bmp = BitmapFactory.decodeFile(imageFile.getAbsolutePath(), bmOptions);
+        //to.do 如果存在预览方向改变，进行图片旋转
+        try {
+            Bitmap bitmap = rotateImage(bmp, imageFile.getAbsolutePath());
+            imageView.setImageBitmap(bitmap);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Bitmap rotateImage(Bitmap bmp, String path) throws Exception{
+        ExifInterface srcExif = new ExifInterface(path);
+        Matrix matrix = new Matrix();
+        int angle = 0;
+        int orientation = srcExif.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                ExifInterface.ORIENTATION_NORMAL);
+        switch (orientation){
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                angle = 90;
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                angle = 180;
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                angle = 270;
+                break;
+            default:
+                break;
+        }
+        matrix.postRotate(angle);
+        return  Bitmap.createBitmap(bmp, 0, 0, bmp.getWidth(), bmp.getHeight(), matrix, true);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_EXTERNAL_STORAGE: {
+                //to.do 判断权限是否已经授予
+                boolean grantedPer = false;
+                for (int i: grantResults ) {
+                    if(i != PackageManager.PERMISSION_GRANTED)
+                        grantedPer = true;
+                }
+
+                if(grantedPer)
+                    Toast.makeText(getApplicationContext(), "需要授权以开启相机", Toast.LENGTH_LONG).show();
+                else
+                    takePicture();
+
+                break;
+            }
+            default:
+                Log.e("Unknown Error", "onRequestPermissionsResult: Unknown Error" );
+        }
+    }
+}
